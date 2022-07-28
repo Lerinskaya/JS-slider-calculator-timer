b954e9d063e4cb439ddd8367514798c2272ea951
Lerinskaya <rammnescence1@gmail.com>
2022-07-28 17:00:33 +0300

    J-1: - scss

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let offset = 0;
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
@@ -24,31 +23,4 @@ left.addEventListener('click', function () {
         left.classList.add("disabled");
     }
     slider.style.left = -offset + 'px';
-=======
-let offset = 0;
-const left = document.querySelector('.left');
-const right = document.querySelector('.right');
-const slider = document.querySelector('.slider');
-
-right.addEventListener('click', function () {
-    left.removeAttribute('disabled');
-    left.classList.remove("disabled");
-    offset += 960;
-    if (offset === 1920) {
-        right.setAttribute('disabled', 'disabled');
-        right.classList.add("disabled");
-    }
-    slider.style.left = -offset + 'px';
-});
-left.addEventListener('click', function () {
-    right.removeAttribute('disabled');
-    right.classList.remove("disabled");
-    offset -= 960;
-    if (offset <= 0) {
-        offset = 0;
-        left.setAttribute('disabled', 'disabled');
-        left.classList.add("disabled");
-    }
-    slider.style.left = -offset + 'px';
->>>>>>> 1194a92c042f7043cfa73b7d32a930b153bbbb70
 });
