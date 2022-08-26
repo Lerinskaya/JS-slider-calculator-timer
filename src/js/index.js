dd05bfb02f5c7ad6e1562b34913371c2292eb68f
Lerinskaya <rammnescence1@gmail.com>
2022-08-26 10:47:05 +0300

    J-3: change

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,11 @@ import "../css/style.scss";
 import "../css/common.scss";
 import "../css/calc.scss";
 import "../css/pomodoro.scss";
+import "../css/weather.scss";
 import "./slider.js";
 import "./calc.js";
 import "./pomodoro.js";
+import "./weather.js";
 
 
 
